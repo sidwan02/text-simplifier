728079744221c4a89aa64b47a44dc35c0e3a29ae
Siddharth Diwan <sidwan02@gmail.com>
2021-12-03 08:10:14 -0500

    created perplexity metric

--- hw4/code/metrics.py
+++ hw4/code/metrics.py
@@ -3,7 +3,7 @@ class AccWeightedSum(tf.keras.metrics.Metric):
     A custom Keras metric to compute the running average of the confusion matrix
     """
     def __init__(self, **kwargs):
-        super(LossPerSymbol,self).__init__(name='loss_per_symbol_metric',**kwargs) # handles base args (e.g., dtype)
+        super(LossPerSymbol,self).__init__(name='acc_weighted_sum_metric',**kwargs) # handles base args (e.g., dtype)
         self.total_tokens = self.add_weight("total_tokens", initializer="zeros")
         self.total_weighted_acc = self.add_weight("total_weighted_acc", initializer="zeros")
         
@@ -46,8 +46,59 @@ class AccWeightedSum(tf.keras.metrics.Metric):
     def result(self):
         return self.total_weighted_acc / self.total_tokens
     
+class Perplexity(tf.keras.metrics.Metric):
+    """
+    A custom Keras metric to compute the running average of the confusion matrix
+    """
+    def __init__(self, **kwargs):
+        super(LossPerSymbol,self).__init__(name='perplexity_metric',**kwargs) # handles base args (e.g., dtype)
+        self.acc_loss = self.add_weight("acc_loss", initializer="zeros")
+        self.total_valid_tokens = self.add_weight("total_valid_tokens", initializer="zeros")
+        
+    def reset_states(self):
+        self.acc_loss.assign(0)
+        self.total_valid_tokens.assign(0)
+            
+    def update_state(self, y_true, y_pred, sample_weight=None): # y_true = labels; y_pred = probs
+        probs, labels = y_pred, y_true
+        mask, batch_valid_tokens = self.create_mask(labels)
+        loss = self.loss_function(probs, labels, mask)
+
+        self.acc_loss.assign_add(loss)
+        self.total_valid_tokens.assign_add(batch_valid_tokens)
+
+
+    def loss_function(self, prbs, labels, mask):
+        """
+        Calculates the model cross-entropy loss after one forward pass
+        Please use reduce sum here instead of reduce mean to make things easier in calculating per symbol accuracy.
+
+        :param prbs:  float tensor, word prediction probabilities [batch_size x window_size x english_vocab_size]
+        :param labels:  integer tensor, word prediction labels [batch_size x window_size]
+        :param mask:  tensor that acts as a padding mask [batch_size x window_size]
+        :return: the loss of the model as a tensor
+        """
+
+        # Note: you can reuse this from rnn_model.
+
+        # assert prbs.shape == (
+        # self.batch_size, self.english_window_size, self.english_vocab_size)
+        # assert labels.shape == (self.batch_size, self.english_window_size)
+        # assert mask.shape == (self.batch_size, self.english_window_size)
+
+        return tf.reduce_sum(tf.boolean_mask(tf.keras.losses.sparse_categorical_crossentropy(
+            labels, prbs), mask))
     
+
     
+    def create_mask(self, labels, eng_padding_index=0):
+        mask = tf.where(labels == eng_padding_index, 0, 1)
+        batch_valid_tokens = tf.cast(tf.math.reduce_sum(mask), dtype=tf.float32)
+        return mask, batch_valid_tokens
+        
+    def result(self):
+        return tf.exp(self.acc_loss / self.total_valid_tokens)
+
 class LossPerSymbol(tf.keras.metrics.Metric):
     """
     A custom Keras metric to compute the running average of the confusion matrix
