698f2ca6e4ca7e49513eb8a34b3a62174cc0e236
William Sun <william.h.sun.88@gmail.com>
2021-12-08 13:29:58 -0500

    Added histogram plotting of length, and more data filtering

--- wikipedia_auto.py
+++ wikipedia_auto.py
@@ -1,5 +1,6 @@
 import json
 import math
+import matplotlib.pyplot as plt
 f = json.load(open('wiki-auto-part-1-data.json'))
 articles = [v for k, v in f.items()]
 simple = []
@@ -7,6 +8,9 @@ normal = []
 
 longest_simple = 0
 longest_normal = 0
+
+normal_cutoff = 400
+simple_cutoff = 200
 
 for d in articles:
     alignments = d["paragraph_alignment"]
@@ -26,22 +30,32 @@ for d in articles:
             if s.startswith(p[1]):
                 normal_par.append(d["normal"]["content"][s])
         
-        normal_text = " ".join(simple_par)
-        simple_text = " ".join(normal_par)
+        normal_text = " ".join(normal_par)
+        simple_text = " ".join(simple_par)
         for x in punctuation:
             normal_text = normal_text.replace(x[0], x[1])
         for x in punctuation:
             simple_text = simple_text.replace(x[0], x[1])
 
+        if len(normal_text.split()) > normal_cutoff or len(simple_text.split()) > simple_cutoff:
+            continue
+
         longest_simple = max(longest_simple, len(simple_text.split()))
         longest_normal = max(longest_normal, len(normal_text.split()))
 
-        simple.append(normal_text)
-        normal.append(simple_text)
+        simple.append(simple_text)
+        normal.append(normal_text)
 
 train_len = math.floor(len(simple)*0.8)
 print("Longest simple line: " + str(longest_simple))
 print("Longest normal line: " + str(longest_normal))
+
+fig, axs = plt.subplots(1, 2, sharey=True, tight_layout=True)
+
+axs[0].hist([len(x.split()) for x in simple], bins=40)
+axs[1].hist([len(x.split()) for x in normal], bins=40)
+
+plt.show()
 
 with open('wiki_simple_train.txt', mode='wt', encoding='utf-8') as myfile:
     myfile.write('\n'.join(simple[:train_len]))
