a4a011e203301fbc3c0f536f51783ac5086da1c6
Siddharth Diwan <sidwan02@gmail.com>
2021-12-08 13:43:29 -0500

    frontend added result text + moved around divs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -23,6 +23,7 @@ function App() {
 
       axios
         .post(
+          // somehow putting the trailing / causes 404
           'https://text-simplifier-api.herokuapp.com/evaluate-model',
           toSend,
           config
@@ -38,25 +39,34 @@ function App() {
 
   return (
     <div className="App">
-      <AwesomeButton
-        type="primary"
-        href="https://tinyurl.com/tb-adam-hyperparam-seq2seq/"
-        target="_blank"
-      >
-        Tensorboard
-      </AwesomeButton>{' '}
-      <form onSubmit={formik.handleSubmit}>
-        <label htmlFor="inputText">Input Text</label>
-        <input
-          id="inputText"
-          name="inputText"
-          type="text"
-          onChange={formik.handleChange}
-          value={formik.values.inputText}
-        />
-        {/* <button type="submit">Submit</button> */}
-        <AwesomeButton type="primary">Get Simplified Text</AwesomeButton>{' '}
-      </form>
+      <div className="whole-div">
+        <form onSubmit={formik.handleSubmit}>
+          <label htmlFor="inputText">Input Text</label>
+          <textarea
+            id="inputText"
+            name="inputText"
+            // type="textArea"
+            onChange={formik.handleChange}
+            value={formik.values.inputText}
+          />
+          {/* <button type="submit">Submit</button> */}
+          <div className="btnDiv">
+            <AwesomeButton type="primary">Get Simplified Text</AwesomeButton>{' '}
+            <AwesomeButton
+              // className="tb-button"
+              type="primary"
+              href="https://tinyurl.com/tb-adam-hyperparam-seq2seq/"
+              target="_blank"
+            >
+              Launch Tensorboard
+            </AwesomeButton>
+          </div>
+        </form>
+        <div className="resultDiv">
+          <div>Output Text</div>
+          <div className="simplified-text-div"></div>
+        </div>
+      </div>
     </div>
   );
 }
