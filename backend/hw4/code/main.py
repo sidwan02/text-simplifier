be5c1346abd89d29cbee1023e23f3d4745d19881
Siddharth Diwan <sidwan02@gmail.com>
2021-12-09 21:26:00 -0500

    storing data in data folder not backend root

--- backend/hw4/code/main.py
+++ backend/hw4/code/main.py
@@ -17,7 +17,7 @@ os.environ["CUDA_VISIBLE_DEVICES"]="-1"
 """
 physical_devices = tf.config.list_physical_devices('GPU') 
 for device in physical_devices:
-    tf.config.experimental.set_memory_growth(device, True)
+	tf.config.experimental.set_memory_growth(device, True)
 """
 
 import datetime
@@ -36,10 +36,10 @@ print("l: ", l)
 ADAM_LR = hp.HParam('adam_lr', hp.Discrete(l.tolist()))
 
 hp.hparams_config(
-    hparams=[ADAM_LR],
-    # https://stackoverflow.com/questions/56852300/hyperparameter-tuning-using-tensorboard-plugins-hparams-api-with-custom-loss-fun
-    metrics=[hp.Metric('AccWeightedSum', display_name='acc_weighted_sum'), hp.Metric('Perplexity', display_name='perplexity')],
-    )
+	hparams=[ADAM_LR],
+	# https://stackoverflow.com/questions/56852300/hyperparameter-tuning-using-tensorboard-plugins-hparams-api-with-custom-loss-fun
+	metrics=[hp.Metric('AccWeightedSum', display_name='acc_weighted_sum'), hp.Metric('Perplexity', display_name='perplexity')],
+	)
 
 
 def train(model, train_complex, train_simple, simple_padding_index):
@@ -200,18 +200,22 @@ def simplify(model, text_input, simplification_strength=1):
 	
 def main():	
 	if len(sys.argv) != 2 or sys.argv[1] not in {"SAVE", "TUNE", "LOAD"}:
-        print("USAGE: python assignment.py <Model Type>")
-        print("<Model Type>: [SAVE/TUNE/LOAD]")
-        exit()
-        
+		print("USAGE: python main.py <Model Type>")
+		print("<Model Type>: [SAVE/TUNE/LOAD]")
+		exit()
+  
+	cur_dir = os.path.dirname(os.path.abspath(__file__))
+	data_root = os.path.dirname(os.path.dirname(cur_dir)) + '/data'
+	print(data_root)
+		
 	UNK_TOKEN = "*UNK*"
 	print("Running preprocessing...")
-	train_simple, test_simple, train_complex, test_complex, simple_vocab, complex_vocab, simple_padding_index = get_data('./wiki_normal_train.txt','./wiki_simple_train.txt','./wiki_normal_test.txt','./wiki_simple_test.txt')
+	train_simple, test_simple, train_complex, test_complex, simple_vocab, complex_vocab, simple_padding_index = get_data(data_root + '/wiki_normal_train.txt', data_root + '/wiki_simple_train.txt', data_root + '/wiki_normal_test.txt', data_root + '/wiki_simple_test.txt')
 	# train_simple, test_simple, train_complex, test_complex, simple_vocab, complex_vocab, simple_padding_index = get_data('./dummy_data/wiki_normal_train.txt','./dummy_data/wiki_simple_train.txt','./wiki_normal_test.txt','./wiki_simple_test.txt')
 	# train_simple, test_simple, train_complex, test_complex, simple_vocab, complex_vocab, simple_padding_index = get_data('./dummy_data/fls.txt','./dummy_data/els.txt','./dummy_data/flt.txt','./dummy_data/elt.txt')
 	print("Preprocessing complete.")
  
-	print("eng_padding_index ======================: ", eng_padding_index)
+	print("simple_padding_index ======================: ", simple_padding_index)
  
 	
 	train_complex = train_complex[:1000, :]
@@ -220,16 +224,16 @@ def main():
 
 	labels = train_simple[:1000, 1:]
 
-    # ============
-    # print("train_complex.shape: ", train_complex.shape)
-    # print("train_simple_trunc.shape: ", train_simple_trunc.shape)
-    # print("labels.shape: ", labels.shape)
+	# ============
+	# print("train_complex.shape: ", train_complex.shape)
+	# print("train_simple_trunc.shape: ", train_simple_trunc.shape)
+	# print("labels.shape: ", labels.shape)
 
 	train_dataset = tf.data.Dataset.from_tensor_slices((train_complex, train_simple_trunc, labels))
-	train_dataset = train_dataset.batch(128)
+	train_dataset = train_dataset.batch(64)
 
-    # model.fit(train_dataset, epochs=10, callbacks=[tensorboard_callback])
-    
+	# model.fit(train_dataset, epochs=10, callbacks=[tensorboard_callback])
+	
 	test_complex = test_complex[:1000, :]
 
 	test_simple_trunc = test_simple[:1000, :-1]
@@ -237,114 +241,114 @@ def main():
 	labels = test_simple[:1000, 1:]
 
 	test_dataset = tf.data.Dataset.from_tensor_slices((test_complex, test_simple_trunc, labels))
-	test_dataset = test_dataset.batch(128)
+	test_dataset = test_dataset.batch(64)
 
 
 	def save_trained_weights(hparams):
-    	model_args = (COMPLEX_WINDOW_SIZE, len(complex_vocab), SIMPLE_WINDOW_SIZE, len(simple_vocab), hparams)
-    	model = Simplifier_Transformer(*model_args) 
-     
-    	model.compile(optimizer=model.optimizer, loss=custom_loss, metrics=[AccWeightedSum(), Perplexity()], run_eagerly=True)
-     
-    	model.fit(
-            train_dataset, 
-            epochs=3, 
-            # callbacks=[
-            #     keras.callbacks.TensorBoard(log_dir=logdir, histogram_freq=1, update_freq='batch', embeddings_freq=1), 
-            #     hp.KerasCallback(logdir, hparams)
-            #     ], 
-            validation_data=test_dataset
-            )
-     
-    	model((np.zeros((128, 14)), np.zeros((128, 14))))
-    	
-    	model.save_weights(cur_dir + "/model.h5")
+		model_args = (COMPLEX_WINDOW_SIZE, len(complex_vocab), SIMPLE_WINDOW_SIZE, len(simple_vocab), hparams)
+		model = Simplifier_Transformer(*model_args) 
+	 
+		model.compile(optimizer=model.optimizer, loss=custom_loss, metrics=[AccWeightedSum(), Perplexity()], run_eagerly=True)
+	 
+		model.fit(
+			train_dataset, 
+			epochs=3, 
+			# callbacks=[
+			#     keras.callbacks.TensorBoard(log_dir=logdir, histogram_freq=1, update_freq='batch', embeddings_freq=1), 
+			#     hp.KerasCallback(logdir, hparams)
+			#     ], 
+			validation_data=test_dataset
+			)
+	 
+		model((np.zeros((128, 14)), np.zeros((128, 14))))
+		
+		model.save_weights(cur_dir + "/model.h5")
 
 	def evaluate_model_from_loaded_weights(hparams):
-        model_args = (COMPLEX_WINDOW_SIZE, len(complex_vocab), SIMPLE_WINDOW_SIZE, len(simple_vocab), hparams)
-        model = Simplifier_Transformer(*model_args)
-        
-        model((np.zeros((128, 14)), np.zeros((128, 14))))
-        
-        model.load_weights(cur_dir + "/model.h5")
-        
-        model.compile(optimizer=model.optimizer, loss=custom_loss, metrics=[AccWeightedSum(), Perplexity()], run_eagerly=True)
-        
-        score = model.evaluate(test_dataset, verbose=0)
+		model_args = (COMPLEX_WINDOW_SIZE, len(complex_vocab), SIMPLE_WINDOW_SIZE, len(simple_vocab), hparams)
+		model = Simplifier_Transformer(*model_args)
+		
+		model((np.zeros((128, 14)), np.zeros((128, 14))))
+		
+		model.load_weights(cur_dir + "/model.h5")
+		
+		model.compile(optimizer=model.optimizer, loss=custom_loss, metrics=[AccWeightedSum(), Perplexity()], run_eagerly=True)
+		
+		score = model.evaluate(test_dataset, verbose=0)
 
-        print("score: ", score)
+		print("score: ", score)
 
-        return score
+		return score
 	
 	def run(hparams, logdir):
-        print("hparams: ", hparams)
+		print("hparams: ", hparams)
 
-        # model_args = (FRENCH_WINDOW_SIZE, len(french_vocab),
-        #             ENGLISH_WINDOW_SIZE, len(english_vocab), hparams)
-        # if sys.argv[1] == "RNN":
-        #     model = RNN_Seq2Seq(*model_args)
-        # elif sys.argv[1] == "TRANSFORMER":
-        # model = Transformer_Seq2Seq(*model_args)
-        model_args = (COMPLEX_WINDOW_SIZE, len(complex_vocab), SIMPLE_WINDOW_SIZE, len(simple_vocab), hparams)
-        model = Simplifier_Transformer(*model_args)
+		# model_args = (FRENCH_WINDOW_SIZE, len(french_vocab),
+		#             ENGLISH_WINDOW_SIZE, len(english_vocab), hparams)
+		# if sys.argv[1] == "RNN":
+		#     model = RNN_Seq2Seq(*model_args)
+		# elif sys.argv[1] == "TRANSFORMER":
+		# model = Transformer_Seq2Seq(*model_args)
+		model_args = (COMPLEX_WINDOW_SIZE, len(complex_vocab), SIMPLE_WINDOW_SIZE, len(simple_vocab), hparams)
+		model = Simplifier_Transformer(*model_args)
 
-        # model.compile(optimizer=model.optimizer, run_eagerly=True)
-        # loss_per_symbol_metric = tf.keras.metrics.Mean(name="loss_per_symbol")
-        # acc_weighted_sum_metric = tf.keras.metrics.Mean(name="acc_weighted_sum")
-        
-        model.compile(optimizer=model.optimizer, loss=custom_loss, metrics=[AccWeightedSum(), Perplexity()], run_eagerly=True)
+		# model.compile(optimizer=model.optimizer, run_eagerly=True)
+		# loss_per_symbol_metric = tf.keras.metrics.Mean(name="loss_per_symbol")
+		# acc_weighted_sum_metric = tf.keras.metrics.Mean(name="acc_weighted_sum")
+		
+		model.compile(optimizer=model.optimizer, loss=custom_loss, metrics=[AccWeightedSum(), Perplexity()], run_eagerly=True)
 
-        # ============
-        # perplexity_metric = tf.keras.metrics.Mean(name="perplexity")
+		# ============
+		# perplexity_metric = tf.keras.metrics.Mean(name="perplexity")
 
-        model.fit(
-            train_dataset, 
-            epochs=3, 
-            callbacks=[
-                keras.callbacks.TensorBoard(log_dir=logdir, histogram_freq=1, update_freq='batch', embeddings_freq=1), 
-                hp.KerasCallback(logdir, hparams)
-                ], 
-            validation_data=test_dataset
-            )
+		model.fit(
+			train_dataset, 
+			epochs=3, 
+			callbacks=[
+				keras.callbacks.TensorBoard(log_dir=logdir, histogram_freq=1, update_freq='batch', embeddings_freq=1), 
+				hp.KerasCallback(logdir, hparams)
+				], 
+			validation_data=test_dataset
+			)
 
 	if sys.argv[1] == "SAVE":
-        ADAM_LR = hp.HParam('adam_lr', hp.Discrete([0.001]))
-        hparams = {
-            'adam_lr': ADAM_LR.domain.values[0],
-        }
+		ADAM_LR = hp.HParam('adam_lr', hp.Discrete([0.001]))
+		hparams = {
+			'adam_lr': ADAM_LR.domain.values[0],
+		}
 
-        save_trained_weights(hparams)
+		save_trained_weights(hparams)
 
 	elif sys.argv[1] == "LOAD":
-        ADAM_LR = hp.HParam('adam_lr', hp.Discrete([0.001]))
+		ADAM_LR = hp.HParam('adam_lr', hp.Discrete([0.001]))
 
-        hparams = {
-            'adam_lr': ADAM_LR.domain.values[0],
-        }
+		hparams = {
+			'adam_lr': ADAM_LR.domain.values[0],
+		}
 
-        evaluate_model_from_loaded_weights(hparams)
+		evaluate_model_from_loaded_weights(hparams)
 
 	elif sys.argv[1] == "TUNE":
-        l = np.arange(0.0001, 0.01 + 0.0001, 0.0005)
-        print("l: ", l)
-        ADAM_LR = hp.HParam('adam_lr', hp.Discrete(l.tolist()))
-        hparams = {
-            'adam_lr': ADAM_LR.domain.values[0],
-        }
-        
-        # av.setup_visualization(enable=True)
-        session_num = 0
+		l = np.arange(0.0001, 0.01 + 0.0001, 0.0005)
+		print("l: ", l)
+		ADAM_LR = hp.HParam('adam_lr', hp.Discrete(l.tolist()))
+		hparams = {
+			'adam_lr': ADAM_LR.domain.values[0],
+		}
+		
+		# av.setup_visualization(enable=True)
+		session_num = 0
 
-        # https://stackoverflow.com/questions/56559627/what-are-hp-discrete-and-hp-realinterval-can-i-include-more-values-in-hp-realin
-        for optimizer in ADAM_LR.domain.values:
-            hparams = {
-                'adam_lr': optimizer,
-            }
-            run_name = "run-%d" % session_num
-            print('--- Starting trial: %s' % run_name)
-            print({h: hparams[h] for h in hparams})
-            run(hparams, hparams_log_dir + run_name)
-            session_num += 1
+		# https://stackoverflow.com/questions/56559627/what-are-hp-discrete-and-hp-realinterval-can-i-include-more-values-in-hp-realin
+		for optimizer in ADAM_LR.domain.values:
+			hparams = {
+				'adam_lr': optimizer,
+			}
+			run_name = "run-%d" % session_num
+			print('--- Starting trial: %s' % run_name)
+			print({h: hparams[h] for h in hparams})
+			run(hparams, hparams_log_dir + run_name)
+			session_num += 1
 	
 	
 if __name__ == '__main__':
